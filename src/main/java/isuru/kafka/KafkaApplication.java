ae72189472435ec1bb2388f8e5c9fc5e5169360f
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-10-02 16:13:10 +0200

    adds sql database

--- src/main/java/isuru/kafka/KafkaApplication.java
+++ src/main/java/isuru/kafka/KafkaApplication.java
@@ -1,5 +1,8 @@
 package isuru.kafka;
 
+import isuru.kafka.entities.ToDoItem;
+import isuru.kafka.services.ToDoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
